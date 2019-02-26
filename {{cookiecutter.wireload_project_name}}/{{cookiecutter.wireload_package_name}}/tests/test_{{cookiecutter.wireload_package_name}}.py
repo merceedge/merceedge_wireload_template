8c42e03bcd66f4b9e8ecf2f070450ba0e923f886
hobo0cn <hobo0cn@gmail.com>
2019-02-26 11:31:26 +0800

    fix mock

--- {{cookiecutter.wireload_project_name}}/{{cookiecutter.wireload_package_name}}/tests/test_{{cookiecutter.wireload_package_name}}.py
+++ {{cookiecutter.wireload_project_name}}/{{cookiecutter.wireload_package_name}}/tests/test_{{cookiecutter.wireload_package_name}}.py
@@ -1,28 +1,24 @@
 import pytest
-from MyWireload_wireload import MyWireload, 
-import my_wireload
+import pytest
+import os
 
-
-class MockEdge:
-    def __init__(self, package_config):
-        self.user_config = package_config
-
-    def wireload_emit_output_payload(output_name, 
-                                    mock_output_call, 
-                                    output_payload):
-        mock_output_call(output_name, output_payload)        
-    
+import {{cookiecutter.wireload_package_name}}
+from merceedge.util.yaml import load_yaml
+from merceedge.util.mock import MockEdge
 
 
 @pytest.mark.run(order=1)
 def test_{{cookiecutter.wireload_package_name}}():
     mock_edge = MockEdge(my_wireload.__config__)
-    new_wireload_obj = {{cookiecutter.wireload_class_name}}(edge=mock_edge,
-                                  model_template_config=)
+    tests_path = os.path.dirname(os.path.realpath(__file__))
+    model_template_yml = load_yaml(os.path.join(tests_path, '../templates', '{{cookiecutter.wireload_class_name}}_template.yml'))
+    new_wireload_obj = MyWireload(edge=mock_edge,
+                                  model_template_config=model_template_yml)
+
     def get_ouput(output_payload):
         # Get output
         assert output_payload is not None
-        
+
     # mock output
     new_wireload_obj.emit_output_call = get_output
     # input
